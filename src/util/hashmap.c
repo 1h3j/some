7f2156440471e70cffc7cb24f02f4109c1f54a05
j3h1 <169068827+j3h1@users.noreply.github.com>
2025-05-16 23:55:48 +0800

    Added dependencyless hashmaps

---
+++ src/util/hashmap.c
@@ -0,0 +1,118 @@
+#include "hashmap.h"
+
+#include "logging.h"
+
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+
+uint64_t hash_fnv1a(uint64_t base, void* input, int len) {
+  uint64_t h = base;
+
+  for (int i=0; i<len; i++) {
+    h ^= (uint64_t) *((uint8_t *)(input + i));
+    h *= HASHMAP_FNV_PRIME;
+  }
+
+  return h;
+}
+
+map_t *map_create(unsigned long size, int layers, unsigned long element_size) {
+  map_t *map = (map_t *)malloc(sizeof(map_t));
+
+  map->layers = (layer_t *)malloc(sizeof(layer_t) * layers);
+
+  map->hash_function = &hash_fnv1a;
+
+  // Extra bytes is for storing the key's pointer
+  element_size += sizeof(void*); 
+
+  map->element_bytes = element_size;
+  map->layers_size = layers;
+
+  for (int l=0; l<layers; l++) {
+    layer_t *layer = map->layers + l;
+    layer->buckets = malloc(element_size * size);
+
+    layer->entries = size;
+
+    memset(layer->buckets, 0, element_size * size);
+  }
+
+  return map;
+}
+
+void map_free(map_t *map) {
+  for (int l=0; l < map->layers_size; l++) {
+    layer_t *layer = map->layers + l;
+
+    for (int e=0; e < layer->entries; e++) {
+      // [ (void *)0x123... | value ]
+      //  ^                   ^
+      //  \                   ptr + sizeof(void*)
+      //  ptr
+
+      void *ptr = layer->buckets + e * map->element_bytes;
+
+      void **key_ptr = (void **) ptr;
+
+      if (*key_ptr != NULL) free(*key_ptr);
+    }
+  }
+
+  free(map->layers);
+  free(map);
+}
+
+void *map_at(map_t *map, void *key, unsigned int key_len) {
+  uint64_t seed = HASHMAP_FNV_BASE;
+
+  void* ptr = NULL;
+
+  for (unsigned int l=0; l < map->layers_size; l++) {
+    layer_t *layer = &(map->layers[l]);
+
+    uint64_t hash = map->hash_function(seed, key, key_len);
+    unsigned int index = (unsigned int)(hash % layer->entries);
+
+    ptr = layer->buckets + (index * map->element_bytes);
+    void **key_ptr_ptr = (void **) ptr;
+
+    if (*key_ptr_ptr == NULL) {
+      *key_ptr_ptr = malloc(key_len + sizeof(unsigned int));
+      *(unsigned int *)(* key_ptr_ptr) = key_len;
+
+      memcpy(*key_ptr_ptr + sizeof(unsigned int), 
+             key, 
+             key_len);
+
+      break;
+    }
+
+    unsigned int keylen_from_map = *(unsigned int *)(*key_ptr_ptr);
+
+    // For optimization, because memcmp is slow.
+    if (*(char *)key == *(char *) (*key_ptr_ptr + sizeof(unsigned int))) {
+      if (memcmp(key, *key_ptr_ptr + sizeof(unsigned int), key_len) == 0) {
+        break;
+      }
+    }
+
+    seed += HASHMAP_SALT;
+    ptr = NULL;
+  }
+
+  if (ptr == NULL) {
+#ifndef SOME_DISABLE_WARNS
+    log_warn("Could not find a spot. (Maybe try increasing map size or layers?)");
+#endif
+    return ptr;
+  }
+
+  return ptr + sizeof(void *);
+}
+
+void map_set(map_t *map, void *key, unsigned int key_len, void *data) {
+  void *ptr = map_at(map, key, key_len);
+  memcpy(ptr, data, map->element_bytes - sizeof(void *));
+}
---
+++ src/util/hashmap.h
@@ -0,0 +1,134 @@
+#ifndef HASHMAP_H
+#define HASHMAP_H
+
+#include <stdint.h>
+#include <stdbool.h>
+
+#if !defined(HASHMAP_DEFAULT_INITIAL_ELEMENTS)
+#define HASHMAP_DEFAULT_INITIAL_ELEMENTS 256
+#endif // HASHMAP_DEFAULT_INITIAL_ELEMENTS
+
+#if !defined(HASHMAP_DEFAULT_INITIAL_LAYERS)
+#define HASHMAP_DEFAULT_INITIAL_LAYERS   2
+#endif // HASHMAP_DEFAULT_INITIAL_LAYERS
+
+#if !defined(HASHMAP_FNV_BASE)
+#define HASHMAP_FNV_BASE  0xcbf29ce484222325
+#endif // HASHMAP_FNV_BASE
+
+#if !defined(HASHMAP_FNV_PRIME)
+#define HASHMAP_FNV_PRIME 0x100000001b3
+#endif // HASHMAP_FNV_PRIME
+
+#if !defined(HASHMAP_SALT)
+#define HASHMAP_SALT      0x01
+#endif // HASHMAP_SALT
+
+#define HASHMAP_VACANT   0
+#define HASHMAP_OCCUPIED 1
+
+/*          | Key (as pointer) | Data  |
+ *          |------------------|-------|
+ * Layer #1=[ (void *)0x123... | value ]   Layer #2=[       NULL       | value ]
+ *          [       NULL       | value ]            [       NULL       | value ]
+ *          [       NULL       | value ]            [       NULL       | value ]
+ *          [       NULL       | value ]            [       NULL       | value ]
+ *          [       NULL       | value ]        ___ [ (void *)0xabc... | value ]
+ *          [       NULL       | value ]       /    [       NULL       | value ]
+ *          [       NULL       | value ] Added salt [       NULL       | value ]
+ *          [ (void *)0x321... | value ] ____/      [       NULL       | value ]
+ *          [       NULL       | value ]            [       NULL       | value ]
+ *          [       NULL       | value ]            [       NULL       | value ]
+ *          [ (void *)0x1a4... | value ]            [       NULL       | value ]
+ *          [       NULL       | value ]            [       NULL       | value ]
+ *          [       NULL       | value ]            [       NULL       | value ]
+ *
+ * If the key pointer is NULL, that means it's unoccupied, the key used to access
+ * the entry is allocated somewhere and pointer is stored next to data. If it is
+ * not NULL, then it will check if the key given is exactly the same as the key on
+ * the table. If it's the same, then it will return like normal. But if it's
+ * already occupied or a collision, then a salt will be added to the seed for the
+ * next layer. And this repeats until it finds the exact key.
+ *
+ * Yes, the number of layers can be infinite, this is designed as a security measure.
+ * It prevents any collisions at all even if it's already got to 5 layers (not that
+ * it would happen, but certainly it *may*).
+ *
+ * 2 Layers are the recommended amount for a very large hash table, but it might
+ * need 3 layers if the hashing algorithm provided is either bad and collides all 
+ * the time, the table is small, or the data takes up 90% of the table, or even
+ * all of the above.
+ *
+ * ======= Structures that cannot be represented in C structs =======
+ *
+ * These structures can't be represented in the standard way of using C structs,
+ * because they're dynamically sized, and it's also packed.
+ *
+ * Bucket -> [ KEYPOINTER, s:sizeof(void *) | Value, s:dynamic ]
+ * Key    -> [ KEYLEN, s:sizeof(unsigned int) | KEYDATA, s:dynamic ]
+*/
+
+/***/
+
+typedef struct {
+  void* buckets;
+  unsigned long entries;
+} layer_t;
+
+typedef struct {
+  layer_t *layers;
+  unsigned int layers_size;
+
+  unsigned int element_bytes;
+
+  uint64_t (*hash_function)(uint64_t, void *, int);
+} map_t;
+
+/**
+ * Implementation of FNV-1a hashing function
+ *
+ * @param base  The "base" or seed of the funciton.
+ * @param prime Changes how the function behaves.
+ * @param data  Data as a pointer.
+ * @param len   Length of data in bytes.
+*/
+uint64_t hash_fnv1a(uint64_t base, void* input, int len);
+
+/**
+ * Create a hash map object
+ *
+ * @param size         Size of an element
+ * @param layers       Number of layers. Recommended amount is 2
+ * @param element_size Size of an element in bytes
+*/
+map_t *map_create(unsigned long size, int layers, unsigned long element_size);
+
+/**
+ * Frees a map object.
+ *
+ * @param map Pointer to a hash map
+*/
+void map_free(map_t *map);
+
+/**
+ * Gives you a pointer to the location of what the key represents.
+ *
+ * @param map     Pointer to a hashmap.
+ * @param key     Pointer to the key data.
+ * @param key_len Size of the key data in bytes.
+ *
+ * @return Pointer assigned to the key in the map.
+*/
+void *map_at(map_t *map, void* key, unsigned int key_len);
+
+/**
+ * Sets the value at a given key.
+ *
+ * @param map     Pointer to a hashmap.
+ * @param key     Pointer to the key data.
+ * @param key_len Size of the key data in bytes.
+ * @param data    Pointer containing the actual data.
+*/
+void map_set(map_t *map, void* key, unsigned int key_len, void* data);
+
+#endif // HASHMAP_H
