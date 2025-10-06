649d75a464a57d2f4ac32f27d09a950c20a698fd
1h3j <221726394+1h3j@users.noreply.github.com>
2025-10-07 05:43:22 +0800

    Added backup define for M_PI

--- src/math/angles.c
+++ src/math/angles.c
@@ -1,6 +1,10 @@
 #include "math/angles.h"
 #include <math.h>
 
+#ifndef M_PI
+#define M_PI 3.14159265358979323
+#endif // M_PI
+
 inline AngleDeg radians_to_degrees(AngleRad radians) {
   return radians * (180.0f / M_PI);
 }
